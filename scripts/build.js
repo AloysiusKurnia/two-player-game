d87589cc19b12fb4eaf4af80eaaa892cd677a844
Aloysius Kurnia Mahendra <aloysius1234@yahoo.co.id>
2023-07-29 06:09:08 +0700

    initial commit!

---
+++ scripts/build.js
@@ -0,0 +1,39 @@
+const esbuild = require('esbuild');
+const fs = require('fs/promises');
+
+async function build(devMode = false, clean = false) {
+    if (clean) {
+        await fs.rm('./dist', { recursive: true, force: true });
+    }
+
+    await esbuild.build({
+        entryPoints: ['src/backend/index.ts'],
+        bundle: true,
+        platform: 'node',
+        target: 'node14.16.0',
+        outfile: 'dist/index.js',
+        external: ['express', 'express-ws'],
+        minify: true,
+        sourcemap: devMode
+    });
+
+    const frontendScriptsDirectory = './src/frontend/scripts';
+    const frontendScriptFiles = (await fs.readdir(frontendScriptsDirectory))
+        .filter(file => file.endsWith('.ts'))
+        .map(file => `${frontendScriptsDirectory}/${file}`);
+
+    await esbuild.build({
+        entryPoints: frontendScriptFiles,
+        bundle: true,
+        platform: 'browser',
+        target: 'es6',
+        outdir: 'dist/static/scripts',
+        minify: true,
+        sourcemap: devMode
+    });
+}
+
+const devMode = process.argv.includes('--dev');
+const clean = process.argv.includes('--clean');
+
+build(devMode, clean);
---
+++ src/backend/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import expressWs from 'express-ws';
+
+const app = expressWs(express()).app;
+const port = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', './src/frontend/views')
+app.get('/', (_, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
